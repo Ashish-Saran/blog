dc1c1be324e6b7a581097438f2035f9e2502d346
Ashish-Saran <saran.ashish@yahoo.com>
2024-01-08 13:41:39 +0530

    css changes

--- frontend/src/components/Loader.jsx
+++ frontend/src/components/Loader.jsx
@@ -4,7 +4,7 @@ import { FallingLines } from "react-loader-spinner";
 const Loader = () => {
   return (
     <FallingLines
-      color="#1e62d0"
+      color="#33a9b0"
       width="100"
       visible={true}
       ariaLabel="falling-lines-loading"
